387e24e462a343abf6d3f61e56f2f6bec92cac02
pedros43 <psequeira.silva@gmail.com>
2021-03-19 10:24:59 +0000

    Criação da DAO Class

---
+++ app/src/main/java/my/app/city_care/dao/NoteDao.kt
@@ -0,0 +1,24 @@
+package my.app.city_care.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import my.app.city_care.entities.Note
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * from note_table ORDER BY title ASC")
+    fun getAlphabetizedNotes(): LiveData<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Query("DELETE FROM note_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM note_table WHERE id == :id")
+    fun getNoteById(id: Int): LiveData<List<Note>>
+}
